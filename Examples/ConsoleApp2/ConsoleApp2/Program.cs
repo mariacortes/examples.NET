24711d97321330072c55d7f26997e7fb46e16a91
QUONEXT\mcortes <mcortes@quonext.com>
2017-08-30 13:50:54 +0200

    Modified project ConsoleApp2 to HelloWorld and show Hello World

--- Examples/ConsoleApp2/ConsoleApp2/Program.cs
+++ Examples/ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
