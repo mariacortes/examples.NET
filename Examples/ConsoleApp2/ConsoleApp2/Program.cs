345364f3e72b6079ab2804317c52a6bedf39c1a5
QUONEXT\mcortes <mcortes@quonext.com>
2017-08-31 09:19:12 +0200

    Write and read datas from console

--- Examples/ConsoleApp2/ConsoleApp2/Program.cs
+++ Examples/ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,7 +10,23 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Console Title";
+            //Console.WriteLine("Hello World!");
+
+            Console.Write("Nombre: ");
+            string name = Console.ReadLine();
+            Console.Write("Apellidos: ");
+            string subname = Console.ReadLine();
+            Console.Write("Edad: ");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Fecha de nacimiento: ");
+            DateTime bornAge = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("\n\n*****************************************************");
+            Console.WriteLine("*******            Datos personales           *******");
+            Console.WriteLine("*****************************************************\n");
+            Console.WriteLine("Nombre completo: " + name + subname);
+            Console.WriteLine("fecha de nacimiento: " + Convert.ToDateTime(bornAge) + "\t Edad: " + age);
             Console.ReadKey();
         }
     }
