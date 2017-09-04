0f52a5b62a2593bfebec14f1de5affad03636c85
QUONEXT\mcortes <mcortes@quonext.com>
2017-09-04 13:29:25 +0200

    Test variable types

--- Examples/ConsoleApp2/ConsoleApp2/Program.cs
+++ Examples/ConsoleApp2/ConsoleApp2/Program.cs
@@ -8,11 +8,16 @@ namespace HelloWorld
 {
     class Program
     {
+        /// <summary>
+        /// Método principal
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.Title = "Console Title";
             //Console.WriteLine("Hello World!");
 
+            /*
             Console.Write("Nombre: ");
             string name = Console.ReadLine();
             Console.Write("Apellidos: ");
@@ -27,6 +32,17 @@ namespace HelloWorld
             Console.WriteLine("*****************************************************\n");
             Console.WriteLine("Nombre completo: " + name + subname);
             Console.WriteLine("fecha de nacimiento: " + Convert.ToDateTime(bornAge) + "\t Edad: " + age);
+            Console.ReadKey();
+            */
+
+            Console.WriteLine("\n\n*****************************************************");
+            Console.WriteLine("*******    Variables Implicitas/Explicitas    *******");
+            Console.WriteLine("*****************************************************\n");
+            var x = 1; //variable implicita
+            int y = 10; //variable explicita
+            var suma = x + y;
+            Console.WriteLine("La suma de x + y es: {0} + {1} = {2}", x, y, suma);
+            Console.WriteLine("El producto de x * y es: {0} * {1} = {2}", x, y, x * y);
             Console.ReadKey();
         }
     }
